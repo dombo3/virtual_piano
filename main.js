10957923d607286992f2cff4fde0172b2ae4261f
Peter Dombovari <dombo.peter@gmail.com>
2020-10-02 12:53:24 +0200

    create virtual piano

---
+++ main.js
@@ -0,0 +1,41 @@
+document.addEventListener("keypress",function(event) {
+    let code = event.code;
+
+    if (code === "KeyA") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    } else if (code === "KeyS") {
+        let audio = new Audio("sound/S.mp3");
+        audio.play();
+    } else if (code === "KeyD") {
+        let audio = new Audio("sound/D.mp3");
+        audio.play();
+    } else if (code === "KeyF") {
+        let audio = new Audio("sound/F.mp3");
+        audio.play();
+    } else if (code === "KeyG") {
+        let audio = new Audio("sound/G.mp3");
+        audio.play();
+    } else if (code === "KeyH") {
+        let audio = new Audio("sound/H.mp3");
+        audio.play();
+    } else if (code === "KeyJ") {
+        let audio = new Audio("sound/J.mp3");
+        audio.play();
+    } else if (code === "KeyW") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    } else if (code === "KeyE") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    } else if (code === "KeyT") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    } else if (code === "KeyY") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    } else if (code === "KeyU") {
+        let audio = new Audio("sound/A.mp3");
+        audio.play();
+    }
+})
